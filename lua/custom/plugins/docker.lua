a320d9d4018617a4cf8cec306de0696607d078bb
Fabio Gomez <fabiogomez11c@gmail.com>
2024-08-16 22:28:21 -0500

    docker pending to fix error

--- lua/custom/plugins/docker.lua
+++ lua/custom/plugins/docker.lua
@@ -1,42 +1,62 @@
 local pickers = require 'telescope.pickers'
 local config = require('telescope.config').values
 local finders = require 'telescope.finders'
+local actions = require 'telescope.actions'
+local action_state = require 'telescope.actions.state'
 local previewers = require 'telescope.previewers'
 local utils = require 'telescope.previewers.utils'
+local log = require('plenary.log'):new()
+log.level = 'debug'
 local M = {}
 
 M.show_docker_images = function(opts)
   pickers
     .new(opts, {
-      finder = finders.new_table {
-        results = {
-          { name = 'yes', value = { 1, 2, 3, 4 } },
-          { name = 'no', value = { 1, 2, 3, 4 } },
-          { name = 'maybe', value = { 1, 2, 3, 4 } },
-          { name = 'lilo', value = { 1, 2, 3, 4 } },
-        },
+      finder = finders.new_async_job {
+        command_generator = function()
+          return { 'docker', 'images', '--format', 'json' }
+        end,
         entry_maker = function(entry)
-          return {
-            value = entry,
-            display = entry.name,
-            ordinal = entry.name,
-          }
+          local parsed = vim.json.decode(entry)
+          if parsed then
+            return {
+              value = parsed,
+              display = parsed.Repository .. ':' .. parsed.Tag,
+              ordinal = parsed.Repository .. ':' .. parsed.Tag,
+            }
+          end
         end,
       },
       sorter = config.generic_sorter(opts),
       previewer = previewers.new_buffer_previewer {
         title = 'Docker Image Details',
         define_preview = function(self, entry)
+          log.debug(entry)
           vim.api.nvim_buf_set_lines(
             self.state.bufnr,
             0,
             0,
             true,
-            vim.tbl_flatten { 'Hello', 'Everyone', '', '```lua', vim.split(vim.inspect(entry.value), '\n'), '```' }
+            vim.tbl_flatten { '# ' .. entry.value.ID, '```lua', vim.split(vim.inspect(entry.value), '\n'), '```' }
           )
           utils.highlighter(self.state.bufnr, 'markdown')
         end,
       },
+      attach_mappings = function(prompt_bufnr)
+        actions.select_default:replace(function()
+          local selection = action_state.get_selected_entry()
+          actions.close(prompt_bufnr)
+          local command = {
+            'edit',
+            'term://docker',
+            'run',
+            '-it',
+            selection.value.Repository,
+          }
+          vim.cmd(vim.fn.join(command, ' '))
+        end)
+        return true
+      end,
     })
     :find()
 end
